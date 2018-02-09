46275dd2d75a715a3648af0356fca782b9e2c581
Freud Romero <abnertorres58@gmail.com>
2018-02-08 19:23:37 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -20,16 +22,36 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos',(req, res) => {
+app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
-    res.satus(400).send(e);
+    res.status(400).send(e);
   });
 });
 
-app.listen(3000, () =>{
-  console.log('Starter on port 3000');
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //Valid id using isValid
+    //404 - send back empty send
+    if(!ObjectID.isValid(id)){
+      res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+      if(!todo){
+        res.status(404).send()
+      }
+
+      res.send({todo});
+    }, (e) => {
+      res.status(400).send();
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
 module.exports = {app};
