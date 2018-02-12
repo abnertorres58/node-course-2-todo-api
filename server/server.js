63093e3708dbdc43d08467b7f8e3201f31c5539d
Freud Romero <abnertorres58@gmail.com>
2018-02-12 08:33:12 -0500

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 require('./config/config');
 
-
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -38,32 +37,30 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  //Valid id using isValid
-    //404 - send back empty send
-    if(!ObjectID.isValid(id)){
-      res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo) => {
-      if(!todo){
-        res.status(404).send()
-      }
-
-      res.send({todo});
-    }).catch((e) => {
-      res.status(400).send();
-    });
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)){
-    res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
-    if(!todo){
+    if (!todo) {
       return res.status(404).send();
     }
 
@@ -73,34 +70,48 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
-app.patch('/todos/:id', (req,res) => {
+app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
-  if(!ObjectID.isValid(id)){
-    res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
 
-  if(_.isBoolean(body.completed) && body.completed){
+  if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
-  }else{
+  } else {
     body.completed = false;
     body.completedAt = null;
   }
 
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-    if(!todo){
+    if (!todo) {
       return res.status(404).send();
     }
 
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
-  });
+  })
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
-  console.log(`Starter up at port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
